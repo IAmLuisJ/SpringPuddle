b374de070dc89a401f48a45785341e560d4a0753
Juarez <a1016060@bestbuy.com>
2022-05-26 14:52:32 -0500

    init

---
+++ src/main/java/com/luisjuarez/vendorpuddle/VendorpuddleApplication.java
@@ -0,0 +1,13 @@
+package com.luisjuarez.vendorpuddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorpuddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorpuddleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisjuarez/vendorpuddle/VendorpuddleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisjuarez.vendorpuddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorpuddleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
